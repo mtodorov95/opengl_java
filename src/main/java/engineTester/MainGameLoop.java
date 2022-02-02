fb0fb6c091bd27eec0232ce68fa64affdd8e6241
mtodorov <mtodorov95@gmail.com>
2022-02-02 18:34:02 +0200

    * vao + vbo

--- src/main/java/engineTester/MainGameLoop.java
+++ src/main/java/engineTester/MainGameLoop.java
@@ -2,9 +2,9 @@ package engineTester;
 
 import org.lwjgl.opengl.GL;
 import renderEngine.DisplayManager;
-
-import static org.lwjgl.opengl.GL11.glClearColor;
-
+import renderEngine.Loader;
+import renderEngine.RawModel;
+import renderEngine.Renderer;
 
 public class MainGameLoop {
 
@@ -19,10 +19,33 @@ public class MainGameLoop {
         // bindings available for use.
         GL.createCapabilities();
 
+        Loader loader = new Loader();
+        Renderer renderer = new Renderer();
+        // OpenGL expects vertices to be defined counter clockwise
+
+        // Rect
+        float[] vertices = {
+                -0.5f, 0.5f, 0f, //v0
+                -0.5f, -0.5f, 0f, //v1
+                0.5f, -0.5f, 0f, //v2
+                0.5f, 0.5f, 0f, //v3
+        };
+
+        int[] indices = {
+                0,1,3, // top left tri (v0,v1,v3)
+                3,1,2 // bottom right tri (v3,v1,v2)
+        };
+
+        RawModel model = loader.loadToVAO(vertices, indices);
+
         while (!DisplayManager.isCloseRequested()){
+            renderer.prepare();
+            //
+            renderer.render(model);
             DisplayManager.updateDisplay();
         }
 
+        loader.cleanUp();
         DisplayManager.destroyDisplay();
     }
 }
